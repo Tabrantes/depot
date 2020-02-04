6d123b5ea01a60db8cc576761de627630fc2ffae
Tiago Abrantes <tiagomgabrantes@gmail.com>
2020-02-04 17:04:51 -0300

    Validation!

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,105 +1,34 @@
-require 'test_helper'
-
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @product = products(:one)
-  end
-
-  test "should get index" do
-    get products_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_product_url
-    assert_response :success
-  end
-
-  test "should create product" do
-    assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    end
-
-    assert_redirected_to product_url(Product.last)
-  end
-
-  test "should show product" do
-    get product_url(@product)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_url(@product)
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
-    assert_redirected_to product_url(@product)
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete product_url(@product)
-    end
-
-    assert_redirected_to products_url
-  end
-end
-def creat
-  @product = Product.new(product_params)
-
-  respond_to do |format|
-    if @product.save
-      format.html { redirect_to @product,
-        notice: 'Product was successfully created.' }
-      format.json {render :show, status: :created,
-        location: @product }
-    else
-      puts @product.erros.full_messages
-      format.html { render :new }
-      format.json { render json: @product.errors,
-        status: :unprocessable_entity }
-    end
-  end
-end
-    end
-  end
-end
-require 'test_helper'
-
+require 'test_helper
 class ProductsControllerTest < ActionDispatch: :IntegrationTest
   setup do
     @product = products(:one)
     @title = "The Great Book #{rand(1000)}"
   end
-  end
-end
-test "should creat product" do
-  assert_difference('Product.count') do
+  test "should create product" do
+    assert_difference('Product.count') do
 
-  post products_url, params: {
-      product: {
-        description: @product.description,
-        image_url: @product.image_url,
-        price: @product.price,
-        title: @title,
+    post products_url, params: {
+        product: {
+           description: @product.description,
+           image_url: @product.image_url,
+           price: @product.price,
+           title: @title,
+        }
       }
-    }
-  end
+    end
 
-  assert_redirect_to product_url(Product.last)
-end
+    assert_redirected_to product_url(Product.last)
+  end
 test "should update product" do
+
   patch product_url(@product), params: {
-      product: {
-        description: @product.description,
-        image_url: @product.image_url,
-        price: @product.price,
-        title: @title,
-      }
-    }
+     product: {
+       description: @product.description,
+       imagem_url: @product.image_url,
+       price: @product.price,
+       title: @title,
+     }
+   }
+
   assert_redirected_to product_url(@product)
-end
-  end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,52 +1,5 @@
-require 'test_helper'
-
-class ProductTest < ActiveSupport::TestCase   # <== this is the start of the class
-  test "product attributes must not be empty" do   # <== this is the start of a block
-    product = Product.new
-    assert product.invalid?
-    assert product.errors[:title].any?
-    assert product.errors[:description].any?
-    assert product.errors[:price].any?
-    assert product.errors[:image_url].any?
-  end
-
-test "product price must be positive" do
-  product = product.new(title:        "My Book Title",
-                        description:  "yyy",
-                        image_url:    "zzz.jpg")
-  product.price = -1
-  assert product.invalid?
-  assert_equal ["must be greater than or equal to 0.01"],
-    product.erros[:price]
-  product.price = 1
-  assert product.valid?
-end
-
-def new_product(image_url)
-  product.new(title:        "My Book Title",
-              description:  "yyy",
-              price:        1,
-              image_url:    image_url)
-end
-
-test "image url" do
-  ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FREDJpg
-           http://a.b.c/x/y/z/fred.gif }
-  bad = %w{ fred.doc fred.gif/more fred.gif.more }
-
-  ok.each do |image_url|
-    assert new_product(image_url).valid?,
-           "#{image_url} shouldn't be invalid"
-  end
-
-  bad.each do |image_url|
-  assert new_product(image_url).invalid?
-         "#{image_url} shouldn't be valid"
-  end
-end
-class ProductTest < ActiveSupport::TestCase
+class ProductTest < ActiveSupport: : TesteCase
   fixtures :products
-#...
 end
 test "product is not valid without a unique title" do
   product = Product.new(title:       products(:ruby).title,
@@ -54,14 +7,11 @@ test "product is not valid without a unique title" do
                         price:       1,
                         image_url:   "fred.gif")
   assert product.invalid?
-  assert_equal ["has already been taken"], product.errors[:title]
+  assert_equal ["has already been taken"], product.erros[:title]
 end
-test "product in not valid without a unique title - i18n" do
+test "product is not valid without a unique title - i18n" do
   product = Product.new(title:       products(:ruby).title,
                         description: "yyy",
                         price:       1,
                         image_url:   "fred.gif")
-  assert product.invalid?
-  assert_equal [I18n.translate('erros.message.taken')],
-               product.errors[:title]
 end
